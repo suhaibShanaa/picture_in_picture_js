11b424f305475b717f8aa3bbd78c645a5e372deb
suahaibShanaa <s.shanaa@classera.com>
2021-09-23 06:59:59 +0300

    first commit

--- script.js
+++ script.js
@@ -1,88 +1,31 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText     = document.getElementById('quote');
-const authorText   = document.getElementById('author');
-const newQuoteBtn   = document.getElementById('new-quote');
-const twitterBtn = document.getElementById('twitter');
-const loader = document.getElementById('loader');
-
-// let apiQuotes = [];
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
 
 
-function showLoadingSpinner(){
-    loader.hidden = false;
-    quoteContainer.hidden = true;
-}
-function removeLoadingSpinner(){
-    quoteContainer.hidden = false;
-    loader.hidden = true;
-}
-// Show New Quote
-function newQuote() {
 
-    showLoadingSpinner();
-    // Pick a random quote from apiQoutes array usinf Math function
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length )];
-    
-    // Check if Author fields is blank and replace it with ''
-    if(!quote.author){
-        authorText.textContent = "--"; 
-    }else{
-        authorText.textContent = quote.author;
+// Prompt to select media stream, pass to videp element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadeddata = () => {
+            videoElement.play();
+        }
+    } catch (error){
+        // Catch Error Here 
+        console.log('whoops, error here:', error);
     }
-
-    // Check Qoute length to determine styling
-    if(quote.text.length > 50 ){
-        quoteText.classList.add('long-quote');
-    }else{
-        quoteText.classList.remove('long-quote');
-    }
-
-    quoteText.textContent  = quote.text;
-    removeLoadingSpinner();
 }
 
+button.addEventListener('click', async () => {
+    //Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
 
+});
 
-// Get Qoutes From API
-// async function getQuoteFromAPI(){
-
-//     showLoadingSpinner();
-//     const apiUrl = 'https://type.fit/api/quotes';
-
-//     try {
-//         const response = await fetch(apiUrl);
-//         apiQuotes = await response.json();
-
-//         newQuote();
-
-//     } catch (error) {
-//         alert(error);
-//         getQuoteFromAPI();
-//     }
-// }
-
-// Tweet Qoute
-function tweetQuote(){
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent} - Classera Larning System`;
-    window.open(twitterUrl, '_blank');
-
-}
-// Evet Listeners
-newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click' , tweetQuote);
-
-
-
-
-
-
-/*****On Load****/
-// getQuoteFromAPI();
-newQuote();
-
-
-// If I need to use localQoutes 
-    // newQuote();
-/*  must change  "apiQuotes" to varaible have a quotes like "localQoute"
-    const quote = localQoute[Math.floor(Math.random() * localQoute.length )];
-*/
+// On Load
+selectMediaStream();
